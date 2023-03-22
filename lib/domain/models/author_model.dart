30c393645850b08b62479b70a81e9ac47e066b8f
AdrianBugala <adbugala@gmail.com>
2023-03-22 13:33:15 +0100

    Add freezed to models and cubits

--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -1,34 +1,23 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'author_model.g.dart';
+part 'author_model.freezed.dart';
 
-@JsonSerializable()
-class AuthorModel {
-  const AuthorModel({
-    required this.id,
-    required this.picture,
-    required this.firstName,
-    required this.lastName,
-  });
+@freezed
+class AuthorModel with _$AuthorModel {
+  const AuthorModel._();
 
-  final int id;
-  final String picture;
-  @JsonKey(name: 'first_name')
-  final String firstName;
-  @JsonKey(name: 'last_name')
-  final String lastName;
+  factory AuthorModel(
+    int id,
+    String picture,
+    @JsonKey(name: 'first_name') String firstName,
+    @JsonKey(name: 'last_name') String lastName,
+  ) = _AuthorModel;
 
   String get name {
     return '$firstName $lastName';
   }
 
-factory AuthorModel.fromJson(Map<String, dynamic> json) =>
+  factory AuthorModel.fromJson(Map<String, dynamic> json) =>
       _$AuthorModelFromJson(json);
-  Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
-
-  // AuthorModel.fromJson(Map<String, dynamic> json)
-  //     : id = json['id'],
-  //       picture = json['picture'],
-  //       firstName = json['first_name'],
-  //       lastName = json['last_name'];
 }
